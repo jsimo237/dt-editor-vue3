8345749669e4848df5e236451bd716c97c438ead
Joel Simo <joel.simo@fronttieres.com>
2024-05-07 11:33:43 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,30 @@
+
+## Customize configuration
+
+See [Vite Configuration Reference](https://vitejs.dev/config/).
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Install Editor files
+
+```sh
+node node_modules/datatables.net-editor/install.js <path-to-editor-zip-files>
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
+
+
---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <button @click="addFirstName"> Add First Name</button>
+</template>
+
+<script setup>
+  import {ref,onMounted} from "vue"
+  // import $ from "jquery"
+  import Editor from "datatables.net-editor";
+
+
+  onMounted(()=>{
+    // console.log(new $.fn.dataTable.Editor)
+  })
+
+
+  const editorFirstName = new Editor({
+      fields: [
+          {
+              label: 'Fist Name:',
+              name: 'firstname',
+              type : 'text',
+          },
+      ]
+  })
+
+  const addFirstName = ()=> {
+    editorFirstName.create({
+        title : "Add First Name"
+    }) //
+  }
+</script>
+
