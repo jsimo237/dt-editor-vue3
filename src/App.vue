b1b4ba5aed496353e0f662318f6b01f305fb0e6f
Joel Simo <joel.simo@fronttieres.com>
2024-05-07 11:58:24 +0100

    Second Commit

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@
   })
 
 
-  const editorFirstName = new Editor({
+  const editor = new Editor({
       fields: [
           {
               label: 'Fist Name:',
@@ -24,7 +24,7 @@
   })
 
   const addFirstName = ()=> {
-    editorFirstName.create({
+    editor.create({
         title : "Add First Name"
     }) //
   }
